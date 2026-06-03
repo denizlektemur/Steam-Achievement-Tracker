3900d88da840d945ca2cc9c59e77bbf48d9896d7
denizlektemur <lektemurdeniz@gmail.com>
2026-06-03 17:02:03 +0200

    Initial commit

---
+++ src/main/java/com/denizlektemur/steamachievementtracker/SteamachievementtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.denizlektemur.steamachievementtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamachievementtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamachievementtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/denizlektemur/steamachievementtracker/SteamachievementtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denizlektemur.steamachievementtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamachievementtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
